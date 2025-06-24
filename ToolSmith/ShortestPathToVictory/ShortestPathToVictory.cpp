b51aee96a3c0effefc3200dc430c18746f6d0daf
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-06-24 17:43:22 +0700

    Upload from web UI: Dijkstra_tasks.zip (as ShortestPathToVictory)

---
+++ ToolSmith/ShortestPathToVictory/ShortestPathToVictory.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int n, m, start, end;
+    cin >> n >> m >> start >> end;
+
+    vector<vector<pair<int, int>>> adj(n + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<int> dist(n + 1, numeric_limits<int>::max());
+    dist[start] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (auto& edge : adj[u]) {
+            int v = edge.first;
+            int weight = edge.second;
+
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    if (dist[end] == numeric_limits<int>::max()) {
+        cout << -1 << endl;
+    } else {
+        cout << dist[end] << endl;
+    }
+
+    return 0;
+}
