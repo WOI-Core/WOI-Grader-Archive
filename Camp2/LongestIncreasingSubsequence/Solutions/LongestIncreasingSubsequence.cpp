75d5e43201077a200f6efbad62d2cdb5728cf2c8
Temicide <athiphatsaramas@gmail.com>
2025-06-24 20:44:47 +0000

    From Tool Smith: dynamic programming_tasks.zip (as LongestIncreasingSubsequence)

--- Camp2/LongestIncreasingSubsequence/Solutions/LongestIncreasingSubsequence.cpp
+++ Camp2/LongestIncreasingSubsequence/Solutions/LongestIncreasingSubsequence.cpp
@@ -6,29 +6,24 @@
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  vector<int> arr(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> arr[i];
-  }
-
-  vector<int> dp(n, 1);
-  for (int i = 1; i < n; ++i) {
-    for (int j = 0; j < i; ++j) {
-      if (arr[i] > arr[j]) {
-        dp[i] = max(dp[i], dp[j] + 1);
-      }
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
     }
-  }
 
-  int max_len = 0;
-  for (int i = 0; i < n; ++i) {
-    max_len = max(max_len, dp[i]);
-  }
+    vector<int> dp(n, 1);
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
 
-  cout << max_len << endl;
+    cout << *max_element(dp.begin(), dp.end()) << endl;
 
-  return 0;
+    return 0;
 }
