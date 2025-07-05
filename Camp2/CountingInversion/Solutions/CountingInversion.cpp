690f776554b408b9cd9aa86a5a0fbf5556c94762
Temicide <athiphatsaramas@gmail.com>
2025-07-05 18:52:58 +0000

    From Tool Smith: sorting_problem.zip (as CountingInversion)

---
+++ Camp2/CountingInversion/Solutions/CountingInversion.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+
+  int inversions = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      if (arr[i] > arr[j]) {
+        inversions++;
+      }
+    }
+  }
+
+  cout << inversions << endl;
+
+  return 0;
+}
